f5f5eed782102341bb4f52ab23e062231740a64e
Joran de Lang <j.delang@azerion.com>
2020-10-02 15:25:56 +0200

    latest changes

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+AFRAME.registerComponent('change-color-on-hover', {
+    schema: {
+      color: {default: 'red'}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;  // <a-box>
+      var defaultColor = el.getAttribute('material').color;
+
+      el.addEventListener('mouseenter', function () {
+        el.setAttribute('color', data.color);
+      });
+
+      el.addEventListener('mouseleave', function () {
+        el.setAttribute('color', defaultColor);
+      });
+      
+      el.addEventListener('click', function (){
+        console.log("click");
+        el.setAttribute('a-sphere', );
+      });
+    }
+  });
